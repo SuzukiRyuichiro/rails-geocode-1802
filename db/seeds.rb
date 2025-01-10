897ff5d1351855f88d55282d25be2816d16a8fd2
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-01-10 12:24:40 +0900

    feat: have some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Le wagon tokyo',
+  address: 'Meguro, Tokyo',
+)
+
+Flat.create!(
+  name: 'Scooter house',
+  address: 'Kanda-nishikicho, Chiyoda, Tokyo',
+)
