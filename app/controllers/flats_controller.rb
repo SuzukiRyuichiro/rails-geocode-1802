cde5d895bd7e91f3fe0290f9ca22ee3dd94d1715
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-01-25 10:30:59 +0900

    add popup

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -5,10 +5,11 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
 
-    @markers = @flats.geocoded.map do |flat|
+    @markers = @flats.geocoded.near('Tokyo', 20).map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { flat: flat })
       }
     end
   end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -18,14 +18,30 @@ export default class extends Controller {
 
     // Add the makers
     this.#addMarkersToMap();
+    this.#fitMapToMarkers();
   }
 
   #addMarkersToMap() {
     // iterate over the array of geolocations
     this.markersValue.forEach((marker) => {
+      // create popup
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html); // Add this
       // create a pin
       // Add that pin to the map
-      new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(this.map);
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map);
     });
+  }
+
+  #fitMapToMarkers() {
+    // iterate over the markers
+    const bounds = new mapboxgl.LngLatBounds();
+    // as I go through the locations, I will update the bounds of the map
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
